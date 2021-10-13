948b9c2c4056cbe859f34102764c54d8313f4c27
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-10-13 15:40:02 -0400

    Functionality + JUnit5 testing added to ProductInventory in exercise44

--- exercise44/src/main/java/baseline/ProductInventory.java
+++ exercise44/src/main/java/baseline/ProductInventory.java
@@ -1,43 +1,101 @@
 package baseline;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.File;
-import java.util.Map;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class ProductInventory {
 
     private Map<String, Product> inventoryMap;
 
-    public void setInventoryFromJson(File jsonFile) {
+    public ProductInventory() {
 
-        // Extract the contents of the jsonFile
-        // Use Gson to convert the jsonFullString to a Map<String, Product>
-        // Assign that map to the this.inventoryMap;
+        this.inventoryMap = new TreeMap<>();
 
     }
 
-    public boolean search(String productName) {
+    public boolean search(File jsonFile, String targetProduct) {
+
+        // Call setInventoryFromJson with the passed jsonFile to set up Map data structure
+        setInventoryFromJson(jsonFile);
 
         // Initalize returnValue to false
+        boolean returnValue = false;
 
         // If the productName exists in the productMap
-        // Print all the attributes of that product
-        // Set return value to true
+        if(inventoryMap.containsKey(targetProduct)) {
+
+            Product requestedProduct = inventoryMap.get(targetProduct);
+
+            // Print all the attributes of that product
+            System.out.printf("Name: %s %n" +
+                    "Price: %.2f %n" +
+                    "Quantity: %d %n",
+                    requestedProduct.getName(), requestedProduct.getPrice(), requestedProduct.getQuantity());
+
+            // Set return value to true
+            returnValue = true;
+
+        }
 
         // Return the returnValue
+        return returnValue;
 
-        return false;
+    }
+
+    private void setInventoryFromJson(File jsonFile) {
+
+        // Convert jsonFile to a jsonString, chopping off the bits we don't want
+        String jsonFullString = getFileContentsAsString(jsonFile);
+        String jsonString = jsonFullString.split("\"products\" : ")[1].split("]}")[0]+"]";
+
+        // Convert jsonString into an ArrayList<Product>
+        Gson gson = new Gson();
+        Type userListType = new TypeToken<ArrayList<Product>>(){}.getType();
+        ArrayList<Product> productList = gson.fromJson(jsonString, userListType);
+
+        // Add all contents of the list to a new Map<String, Product>
+        Map<String, Product> newInventoryMap = new TreeMap<>();
+        for (Product cur : productList) {
+            newInventoryMap.put(cur.getName(), cur);
+        }
+
+        // Assign the newInventoryMap as the objects main inventoryMap
+        this.inventoryMap = newInventoryMap;
+
     }
 
     private String getFileContentsAsString(File targetFile) {
 
+
         // For use specifically in the setInventoryFromJson method
 
         // Create StringBuilder for output
-        // Create Scanner for targetFile
-        // Scan every line of targetFile, adding to the output StringBuilder
-        // Returns the StringBuilder as a regular String
+        StringBuilder stringBuilder = new StringBuilder();
 
-        return null;
+        // Create Scanner for targetFile
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(targetFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Scan every line of targetFile, adding to the output StringBuilder
+        while(fileScanner.hasNextLine()) stringBuilder.append(fileScanner.nextLine());
+
+        // Returns the StringBuilder as a regular String
+        return stringBuilder.toString();
+
     }
 
 
---
+++ exercise44/src/test/java/baseline/ProductInventoryTest.java
@@ -0,0 +1,62 @@
+package baseline;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("ProductInventory")
+class ProductInventoryTest {
+
+    private ProductInventory testInventory;
+
+    @BeforeEach
+    void setUp() {
+
+        testInventory = new ProductInventory();
+
+    }
+
+    @Test
+    void search() {
+
+        assertTrue(testInventory.search(
+                new File(Paths.get("exercise44_input.json").toUri()),
+                "Widget"),
+                "1-1. Widget Test"
+        );
+        assertTrue(testInventory.search(
+                        new File(Paths.get("exercise44_input.json").toUri()),
+                        "Thing"),
+                "1-2. Thing Test"
+        );
+        assertTrue(testInventory.search(
+                        new File(Paths.get("exercise44_input.json").toUri()),
+                        "Doodad"),
+                "1-3. Thing Test"
+        );
+
+        assertFalse(testInventory.search(
+                        new File(Paths.get("exercise44_input.json").toUri()),
+                        "iPad"),
+                "2-1. iPad Test"
+        );
+        assertFalse(testInventory.search(
+                        new File(Paths.get("exercise44_input.json").toUri()),
+                        "iPhone"),
+                "2-2. iPhone Test"
+        );
+        assertFalse(testInventory.search(
+                        new File(Paths.get("exercise44_input.json").toUri()),
+                        "iPod"),
+                "2-3. iPod Test"
+        );
+
+
+    }
+}
